52042ced4db35961e1d609077a0497433a8297f2
Michal <michalldbs@seznam.cz>
2024-03-31 22:52:29 +0200

    Change icon, favicon

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -11,5 +11,5 @@
     <a target="_blank" class="btn" href="{PUBLIC_CDN_URL + data.id}"><Icon size="11" icon="open_in_new_tab" />&nbsp;Raw code</a>
 {:else}
     <p>This paste doesn't exist.</p>
-    <a class="btn" href="/"><Icon size="11" icon="open_in_new_tab" />&nbsp;Back to main page</a>
+    <a class="btn" href="/"><Icon size="11" icon="back" />&nbsp;Back to main page</a>
 {/if}
