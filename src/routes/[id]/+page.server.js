a93ce4d686d438301aeb5cebd4e56633bc5d4496
Michal <michalldbs@seznam.cz>
2024-04-01 00:06:46 +0200

    Loading from public url instead of S3 API

--- src/routes/[id]/+page.server.js
+++ src/routes/[id]/+page.server.js
@@ -1,26 +1,31 @@
-import {GetObjectCommand, S3Client} from "@aws-sdk/client-s3";
-import { PRIVATE_AWS_KEY, PRIVATE_AWS_ACCESS, PRIVATE_AWS_PATH, PRIVATE_AWS_BUCKET, PRIVATE_LENGHT_ID } from '$env/static/private'
+//import {GetObjectCommand, S3Client} from "@aws-sdk/client-s3";
+//import { PRIVATE_AWS_KEY, PRIVATE_AWS_ACCESS, PRIVATE_AWS_PATH, PRIVATE_AWS_BUCKET, PRIVATE_LENGHT_ID } from '$env/static/private'
 import hljs from 'highlight.js';
+import { PUBLIC_CDN_URL } from '$env/static/public'
+
 
 export async function load({ params }) {
-    const client = new S3Client({
-        endpoint: "https://eu2.contabostorage.com",
-        region: "eu",
-        credentials: {
-            accessKeyId: PRIVATE_AWS_KEY,
-            secretAccessKey: PRIVATE_AWS_ACCESS,
-        },
-        forcePathStyle: true
-    });
-
-    const command = new GetObjectCommand({
-        Bucket: PRIVATE_AWS_BUCKET,
-        Key: PRIVATE_AWS_PATH + params.id,
-    });
+    // const client = new S3Client({
+    //     endpoint: "https://eu2.contabostorage.com",
+    //     region: "eu",
+    //     credentials: {
+    //         accessKeyId: PRIVATE_AWS_KEY,
+    //         secretAccessKey: PRIVATE_AWS_ACCESS,
+    //     },
+    //     forcePathStyle: true
+    // });
+    //
+    // const command = new GetObjectCommand({
+    //     Bucket: PRIVATE_AWS_BUCKET,
+    //     Key: PRIVATE_AWS_PATH + params.id,
+    // });
 
     try {
-        let res = await client.send(command);
-        let text = await res.Body.transformToString();
+        // let res = await client.send(command);
+        // let text = await res.Body.transformToString();
+        let response = await fetch(PUBLIC_CDN_URL + params.id);
+        let text = await response.text();
+
         let highlightedCode;
 
         if(text) {
